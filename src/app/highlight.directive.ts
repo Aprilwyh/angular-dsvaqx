22b76c2fb7d8ce6121bb7457e10cfe1d99cbe070
Aprilwyh <1416402295@qq.com>
2019-12-02 18:08:32 +0800

    属性型指令

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,12 +1,14 @@
 /* tslint:disable:member-ordering */
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
+// Angular 会在模板中定位每个拥有名叫 appHighlight 属性的元素，并且为这些元素加上本指令的逻辑。
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private el: ElementRef) { }
+  // 使用 ElementRef 来注入宿主 DOM 元素的引用，也就是你放置 appHighlight 的那个元素。
+  constructor(private el: ElementRef) {}
 
   @Input() defaultColor: string;
 
@@ -20,6 +22,7 @@ export class HighlightDirective {
     this.highlight(null);
   }
 
+  // ElementRef通过其nativeElement属性给你了直接访问宿主DOM元素的能力
   private highlight(color: string) {
     this.el.nativeElement.style.backgroundColor = color;
   }
